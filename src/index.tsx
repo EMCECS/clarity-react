b493e2f2d62e692a253286eeff10e6c65cccecdf
Ben Schumacher <Ben.Schumacher@emc.com>
2019-03-01 13:18:34 -0600

    Remove clarity reference.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import "../node_modules/@dell/clarity-ui/dell-clarity-ui.min.css";
-
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
