adf35faa2c97ca4c22e48951daaac27ce141b654
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-15 12:20:42 -0500

    Remove redundant icon imports

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,5 @@
 import {addDecorator, configure} from '@storybook/react';
-import "@clr/icons/clr-icons.min.css";
-import "@clr/icons/clr-icons-lite.min.js";
 import "@clr/ui/clr-ui.min.css"
-import "@clr/icons/clr-icons.min";
-import "@clr/icons/shapes/technology-shapes.js";
 import {withInfo} from "@storybook/addon-info";
 
 const req = require.context('../src', true, /\.stories\.tsx$/);
