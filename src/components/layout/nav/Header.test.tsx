44bb4b43e61b92eb3a6d86a42f2126c0e8f8a394
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-15 12:20:18 -0500

    Remove dupes from prior merges

--- src/components/layout/nav/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Header from "./Header";
-
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    //ReactDOM.render(<Header onToggle={void} onCloseAll={void} />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
