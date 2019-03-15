d50c1d5d9b1b41ec5c321c9c4484171b7cd805cd
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-15 12:20:23 -0500

    Add icon stories

--- src/icon/Icon.stories.tsx
+++ src/icon/Icon.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import {action} from '@storybook/addon-actions';
-import Icon from "./Icon";
+import Icon, {Direction} from "./Icon";
 
 storiesOf('Icon', module)
     .add('a simple icon', () => (
         <Icon shape="home"/>
+    ))
+    .add('with direction "right"', () => (
+        <Icon shape="home" dir={Direction.RIGHT}/>
     ))
 ;
