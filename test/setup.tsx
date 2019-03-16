2f29133a8f4e782f894f09dd129f39a5ed7353b0
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-15 19:18:32 -0500

    Rename setup so that it isn’t run by test suite

---
+++ test/setup.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+// React 16 Enzyme adapter
+Enzyme.configure({adapter: new Adapter()});
