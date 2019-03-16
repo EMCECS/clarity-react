f9d60906fcc99b2823fc6255da3a024ee04672f8
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-16 13:29:24 -0500

    Config fixes

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,13 +6,13 @@ module.exports = ({config}) => {
 
   config.resolve.extensions.push('.ts', '.tsx');
 
-  // config.externals = {
-  //   'jsdom': 'window',
-  //   'cheerio': 'window',
-  //   'react/lib/ExecutionEnvironment': true,
-  //   'react/lib/ReactContext': 'window',
-  //   'react/addons': true,
-  // };
+  config.externals = {
+    'jsdom': 'window',
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': 'window',
+    'react/addons': true,
+  };
 
   return config;
 };
