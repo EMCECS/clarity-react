5eadc475ca21ac423a01306343f752ae443a7fbd
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-16 00:14:56 -0500

    Remove unneeded constructor and clean up

--- src/layout/nav/NavLink.tsx
+++ src/layout/nav/NavLink.tsx
@@ -3,13 +3,12 @@ import * as React from "react";
 interface NavLinkProps {}
 
 export class NavLink extends React.PureComponent<NavLinkProps> {
-    constructor(props: NavLinkProps) {
-        super(props);
-    }
     render() {
         return (
             <a href="#" className="nav-link">
-                <div className="nav-text">{this.props.children}</div>
+                <div className="nav-text">
+                    {this.props.children}
+                </div>
             </a>
         );
     }
