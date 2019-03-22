f2ff0273b03189130ff539206a47fe2757789524
Ben Schumacher <ben.schumacher@dell.com>
2019-03-22 14:34:02 -0500

    First attempt at porting dropdown component to React.

---
+++ src/forms/dropdown/ClassNames.tsx
@@ -0,0 +1,13 @@
+export class ClassNames {
+    public static DROPDOWN: string = "dropdown";
+    public static OPEN: string = "open";
+    public static BUTTON: string = "btn";
+    public static BUTTON_OUTLINE_PRIMARY: string = "btn-outline-primary";
+    public static ACTIVE: string = "active";
+    public static DROPDOWN_TOGGLE: string = "dropdown-toggle";
+    public static DROPDOWN_ITEM: string = "dropdown-item";
+    public static DROPDOWN_HEADER: string = "dropdown-header";
+    public static DROPDOWN_HEADER_CHILD: string = "dropdown-header-child";
+    public static DROPDOWN_DIVIDER: string = "dropdown-divider";
+    public static EXPANDABLE: string = "expandable";
+}
