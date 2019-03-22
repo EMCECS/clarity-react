8e227f071170483db054022cc8933e2cf7275d4a
Aaron Spiegel <aaron.spiegel@emc.com>
2019-03-22 12:32:59 -0500

    Apply license to files
    includes both package.json and license headers

--- src/layout/nav/Header.test.tsx
+++ src/layout/nav/Header.test.tsx
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 import {Header, NavHeaderProps} from ".";
 import {shallow, mount, ShallowWrapper} from "enzyme";
 import * as React from "react";
