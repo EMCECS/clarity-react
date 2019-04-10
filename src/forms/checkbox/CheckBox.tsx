8e7f9e9402197b2c7c6b20ca5c136e54f0947cd0
Ben Schumacher <ben.schumacher@dell.com>
2019-04-10 10:36:23 -0500

    Added checkbox component.

---
+++ src/forms/checkbox/CheckBox.tsx
@@ -0,0 +1,156 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import * as React from "react";
+import {UID} from "react-uid";
+import {Icon} from "../../icon";
+import {ClassNames} from "./ClassNames";
+import {classNames} from "../../utils";
+
+type CheckboxValue = boolean | "indeterminate";
+
+type FormControlProps = {
+    label?: string;
+    inline?: boolean;
+    error?: boolean;
+    disabled?: boolean;
+};
+
+export class FormControl extends React.PureComponent<FormControlProps> {
+    render() {
+        const {disabled, label, error, inline, children} = this.props;
+        return (
+            <div
+                className={classNames([
+                    ClassNames.CLR_FORM_CONTROL, // prettier
+                    disabled && ClassNames.CLR_FORM_CONTROL_DISABLED,
+                ])}
+            >
+                {label && <label className={ClassNames.CLR_CONTROL_LABEL}>{label}</label>}
+                <div
+                    className={classNames([
+                        ClassNames.CLR_CONTROL_CONTAINER, //prettier
+                        error && ClassNames.CLR_ERROR,
+                        inline && ClassNames.CLR_CONTROL_INLINE,
+                    ])}
+                >
+                    {disabled ? FormControl.renderChildrenDisabled(children) : children}
+                </div>
+            </div>
+        );
+    }
+
+    private static renderChildrenDisabled(children: React.ReactNode): React.ReactNode {
+        const childrenDisable = React.Children.map(children, child => {
+            const childEl = child as React.ReactElement;
+            if (childEl.type === CheckBox) {
+                return React.cloneElement(childEl as React.ReactElement<HTMLInputElement>, {
+                    disabled: true,
+                });
+            }
+        });
+        return childrenDisable;
+    }
+}
+
+type SubTextWrapperProps = {
+    shape?: string;
+    text?: string;
+};
+
+export const SubTextWrapper: React.FunctionComponent<SubTextWrapperProps> = ({shape, text, children}) => {
+    return (
+        <div className={ClassNames.CLR_SUBTEXT_WRAPPER}>
+            {shape && <Icon className={ClassNames.CLR_VALIDATE_ICON} shape={shape} />}
+            <span className={ClassNames.CLR_SUBTEXT}>{text}</span>
+        </div>
+    );
+};
+
+type CheckBoxProps = {
+    label?: string;
+    name?: string;
+    id?: string;
+    checked?: CheckboxValue;
+    defaultChecked?: CheckboxValue;
+    ariaLabelledby?: string;
+    disabled?: boolean;
+    onChange?: (newValue: CheckboxValue) => void;
+};
+
+type CheckBoxState = {
+    value: CheckboxValue;
+};
+
+export class CheckBox extends React.PureComponent<CheckBoxProps, CheckBoxState> {
+    private myRef = React.createRef<HTMLInputElement>();
+
+    constructor(props: CheckBoxProps) {
+        super(props);
+        this.state = {value: this.getValue()};
+    }
+
+    getValue(): CheckboxValue {
+        const {checked, defaultChecked} = this.props;
+        const value = this.state ? this.state.value : undefined;
+        if (value !== undefined) return value;
+        return checked ? checked : defaultChecked ? defaultChecked : false;
+    }
+
+    componentDidMount() {
+        (this.myRef.current as HTMLInputElement).indeterminate = this.getValue() === "indeterminate";
+    }
+
+    componentDidUpdate() {
+        (this.myRef.current as HTMLInputElement).indeterminate = this.getValue() === "indeterminate";
+    }
+
+    handleChange(evt: React.ChangeEvent<HTMLInputElement>) {
+        const hardValue = this.props.checked !== undefined;
+        const checked = evt.target.checked;
+        const indeterminate = evt.target.indeterminate;
+        const newValue = indeterminate ? "indeterminate" : checked;
+
+        if (hardValue) {
+            evt.preventDefault();
+            if (this.props.onChange) this.props.onChange(newValue);
+        } else {
+            this.setState({value: newValue});
+        }
+    }
+
+    render() {
+        const {label, name, ariaLabelledby, disabled} = this.props;
+        const setId = this.props.id;
+        const {value} = this.state;
+        return (
+            <UID>
+                {id => (
+                    <div className={ClassNames.CLR_CHECKBOX_WRAPPER}>
+                        <input
+                            type="checkbox" // prettier
+                            id={setId ? setId : id}
+                            name={name}
+                            ref={this.myRef}
+                            checked={value !== false}
+                            onChange={this.handleChange.bind(this)}
+                            className={ClassNames.CLR_CHECKBOX}
+                            aria-labelledby={ariaLabelledby}
+                            disabled={disabled}
+                        />
+                        <label className={ClassNames.CLR_CONTROL_LABEL} htmlFor={setId ? setId : id}>
+                            {label}
+                        </label>
+                    </div>
+                )}
+            </UID>
+        );
+    }
+}
---
+++ src/forms/checkbox/ClassNames.tsx
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+export class ClassNames {
+    public static CLR_FORM_CONTROL: string = "clr-form-control";
+    public static CLR_FORM_CONTROL_DISABLED: string = "clr-form-control-disabled";
+    public static CLR_CONTROL_LABEL: string = "clr-control-label";
+    public static CLR_CONTROL_CONTAINER: string = "clr-control-container";
+    public static CLR_CONTROL_INLINE: string = "clr-control-inline";
+    public static CLR_SUBTEXT_WRAPPER: string = "clr-subtext-wrapper";
+    public static CLR_VALIDATE_ICON: string = "clr-validate-icon";
+    public static CLR_SUBTEXT: string = "clr-subtext";
+    public static CLR_ERROR: string = "clr-error";
+    public static CLR_CHECKBOX_WRAPPER: string = "clr-checkbox-wrapper";
+    public static CLR_CHECKBOX: string = "clr-checkbox";
+}
