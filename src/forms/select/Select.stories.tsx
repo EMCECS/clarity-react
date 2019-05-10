8887da61eedb6c04f0132f1c2fd0c7f308b1fca7
Ben Schumacher <ben-schumacher@users.noreply.github.com>
2019-05-10 13:55:19 -0500

    Feature select component (#7)
    Adding new select component implemented in React.

---
+++ src/forms/select/Select.stories.tsx
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import * as React from "react";
+import {storiesOf} from "@storybook/react";
+import {Select, SelectOption} from "./Select";
+import {action} from "@storybook/addon-actions";
+
+storiesOf("Select", module)
+    .add("Basic select", () => (
+        <div>
+            <Select onChange={action("basic change")}>
+                <SelectOption value="1">One</SelectOption>
+                <SelectOption value="2">Two</SelectOption>
+                <SelectOption value="3">Three</SelectOption>
+            </Select>
+        </div>
+    ))
+    .add("Labels", () => (
+        <div>
+            <Select onChange={action("label select - change")} label="I've got some options">
+                <SelectOption value="1">One</SelectOption>
+                <SelectOption value="2">Two</SelectOption>
+                <SelectOption value="3">Three</SelectOption>
+            </Select>
+        </div>
+    ))
+    .add("Full select display", () => (
+        <div>
+            <Select
+                onChange={action("full select - change")}
+                defaultHelperText="You have these choices"
+                errorHelperText="This field is required!"
+                label="I've got some options"
+            >
+                <SelectOption value="1">One</SelectOption>
+                <SelectOption value="2">Two</SelectOption>
+                <SelectOption value="3">Three</SelectOption>
+            </Select>
+        </div>
+    ))
+    .add("Select with error", () => (
+        <div>
+            <Select
+                error={true}
+                onBlur={action("select with error - blur")}
+                onChange={action("select with error - change")}
+                defaultHelperText="You have these choices"
+                errorHelperText="This field is required!"
+                label="I've got some options"
+            >
+                <SelectOption value="1">One</SelectOption>
+                <SelectOption value="2">Two</SelectOption>
+                <SelectOption value="3">Three</SelectOption>
+            </Select>
+        </div>
+    ));
