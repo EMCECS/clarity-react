75b321e90965f2fdd75460d4c745237da47fbae2
Ben Schumacher <ben-schumacher@users.noreply.github.com>
2019-06-27 12:10:24 -0500

    Adding support for input icons with additional properties and render … (#20)
    * Adding support for input icons with additional properties and render logic.
    * Rename index.tsx to Input.tsx and fixed storybook import.

--- src/forms/input/Input.stories.tsx
+++ src/forms/input/Input.stories.tsx
@@ -8,33 +8,24 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import * as React from 'react';
-import {storiesOf} from '@storybook/react';
-import {action} from '@storybook/addon-actions';
-import {Input} from "./index";
+import * as React from "react";
+import {storiesOf} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+import {Input} from "./Input";
+import {Icon} from "../../icon";
 
-storiesOf('Input', module)
-    .add('a simple input box', () => (
-        <Input name="somevalue" onChange={action('changed')}/>
+storiesOf("Input", module)
+    .add("a simple input box", () => <Input name="somevalue" onChange={action("changed")} />)
+    .add("with a label", () => <Input name="somevalue" label="somevalue" onChange={action("changed")} />)
+    .add("with placeholder text", () => (
+        <Input name="somevalue" placeholder="stuff goes here" onChange={action("changed")} />
     ))
-    .add('with a label', () => (
-        <Input name="somevalue"
-               label="somevalue"
-               onChange={action('changed')}/>
+    .add("with helper text", () => (
+        <Input name="somevalue" helperText="this should help you figure it out" onChange={action("changed")} />
     ))
-    .add('with placeholder text', () => (
-        <Input name="somevalue"
-               placeholder="stuff goes here"
-               onChange={action('changed')}/>
-    ))
-    .add('with helper text', () => (
-        <Input name="somevalue"
-               helperText="this should help you figure it out"
-               onChange={action('changed')}/>
-    ))
-    .add('when disabled', () => (
-        <Input name="somevalue"
-               disabled={true}
-               onChange={action('changed')}/>
-    ))
-;
+    .add("when disabled", () => <Input name="somevalue" disabled={true} onChange={action("changed")} />)
+    .add("input box with icon", () => (
+        <Input name="somevalue" onChange={action("changed")} placeholder="Type to search ...">
+            <Icon shape="search" style={{marginLeft: "-20px", width: "16px", height: "16px"}} />
+        </Input>
+    ));
