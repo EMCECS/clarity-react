5690fe520a422eba6b1beae006086df7cbd824ec
Ben Schumacher <ben-schumacher@users.noreply.github.com>
2019-07-02 08:04:00 -0500

    Created new Table component in React. (#23)
    Created new Table component in React.

---
+++ src/tables/ClassNames.tsx
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+export class ClassNames {
+    public static TABLE: string = "table";
+    public static TABLE_VERTICAL: string = "table-vertical";
+    public static TABLE_NO_BORDER: string = "table-noborder";
+    public static TABLE_COMPACT: string = "table-compact";
+}
