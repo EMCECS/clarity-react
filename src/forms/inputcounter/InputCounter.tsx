439abb35e3f3e35a736090a314f37cd68159a856
gdudhe <girish.dudhe2@emc.com>
2019-07-17 17:25:12 +0530

    Addressing the comment given by Arpita, Priya and Payal

--- src/forms/inputcounter/InputCounter.tsx
+++ src/forms/inputcounter/InputCounter.tsx
@@ -11,13 +11,13 @@ import * as React from "react";
 import * as utils from "../../utils";
 import {Icon} from "../../icon";
 import {Input} from "../input/Input";
+import "./InputCounter.css";
 
 type InputCounterProps = {
     className?: string;
     maxValue?: number;
     minValue?: number;
     defaultValue?: number;
-    name?: string;
     errMsg?: string;
     width?: string;
     onChange?: (evt: React.ChangeEvent<HTMLInputElement>) => void;
@@ -32,36 +32,33 @@ export class InputCounter extends React.PureComponent<InputCounterProps, InputCo
     public static defaultProps: Partial<InputCounterProps> = {
         minValue: Number.MIN_SAFE_INTEGER,
         maxValue: Number.MAX_SAFE_INTEGER,
+        errMsg: "Value should be",
+        width: "6rem",
     };
     constructor(props: any) {
         super(props);
 
+        //Default Value should be >= to minValue
         let defaultValue = isNaN(props.defaultValue) ? 0 : Math.max(props.defaultValue, props.minValue);
+        //Default Value should be <= to minValue
+        defaultValue = Math.min(defaultValue, props.maxValue);
         this.state = {
             value: defaultValue,
             errorMsg: "",
         };
     }
 
-    private getErrorMessage() {
-        if (this.props.errMsg === undefined) {
-            return "Value should be ";
-        } else {
-            return this.props.errMsg;
-        }
-    }
-
     private handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
         if (isNaN(parseInt(evt.target.value))) {
             this.setState({
-                value: isNaN(this.props.minValue!) ? 0 : this.props.minValue!,
+                value: isNaN(this.props.minValue!) ? 0 : Math.max(this.props.defaultValue, this.props.minValue!),
             });
         } else if (parseInt(evt.target.value) > this.props.maxValue!) {
-            let maxValueError = this.getErrorMessage() + " <= " + this.props.maxValue;
+            let maxValueError = this.props.errMsg + " <= " + this.props.maxValue;
             this.setState({errorMsg: maxValueError});
             this.setState({value: parseInt(evt.target.value)});
         } else if (parseInt(evt.target.value) < this.props.minValue!) {
-            let minValueError = this.getErrorMessage() + " >= " + this.props.minValue;
+            let minValueError = this.props.errMsg + " >= " + this.props.minValue;
             this.setState({errorMsg: minValueError});
             this.setState({value: parseInt(evt.target.value)});
         } else {
@@ -73,56 +70,40 @@ export class InputCounter extends React.PureComponent<InputCounterProps, InputCo
     };
 
     private handleIncrement() {
-        if (this.props.maxValue === undefined) {
-            this.setState({value: this.state.value + 1});
-        } else if (this.state.value < this.props.maxValue!) {
+        if (this.state.value < this.props.maxValue!) {
             this.setState({value: this.state.value + 1});
         } else {
-            let maxValueError = this.getErrorMessage() + " <= " + this.props.maxValue;
+            let maxValueError = this.props.errMsg + " <= " + this.props.maxValue;
             this.setState({errorMsg: maxValueError});
         }
     }
 
     private handleDecrement() {
-        if (this.props.minValue === undefined) {
-            this.setState({value: this.state.value - 1});
-        } else if (this.state.value > this.props.minValue!) {
+        if (this.state.value > this.props.minValue!) {
             this.setState({value: this.state.value - 1});
         } else {
-            let minValueError = this.getErrorMessage() + " >= " + this.props.minValue;
+            let minValueError = this.props.errMsg + " > " + this.props.minValue;
             this.setState({errorMsg: minValueError});
         }
     }
 
     render() {
-        const {maxValue, minValue, defaultValue, onChange, name, errMsg} = this.props;
-        let {width} = this.props;
-        if (width === undefined) {
-            width = "6rem"; //This is defaultWidth
-        }
+        const {width, maxValue, minValue, defaultValue, onChange, errMsg} = this.props;
         return (
             <div className="clr-form" style={{width: width}}>
-                <div style={{display: "flex"}}>
-                    <div style={{display: "inline-block", paddingRight: "0%", paddingLeft: "0%"}}>
+                <div className="ic-flex">
+                    <div className="ic-div">
                         <input
+                            className="ic-input"
                             name="counter"
                             value={this.state.value}
                             onChange={this.handleChange.bind(this)}
-                            style={{height: "52px", width: "100%", border: "1px solid #d3d3d3"}}
                         />
                     </div>
-                    <div style={{display: "inline-block"}}>
-                        <Icon
-                            shape="plus"
-                            style={{height: "26px", border: "1px solid #d3d3d3", paddingRight: "0%", paddingLeft: "0%"}}
-                            onClick={this.handleIncrement.bind(this)}
-                        />
+                    <div className="ic-div">
+                        <Icon className="ic-icon" shape="plus" onClick={this.handleIncrement.bind(this)} />
                         <br />
-                        <Icon
-                            shape="minus"
-                            style={{height: "26px", border: "1px solid #d3d3d3", paddingRight: "0%", paddingLeft: "0%"}}
-                            onClick={this.handleDecrement.bind(this)}
-                        />
+                        <Icon className="ic-icon" shape="minus" onClick={this.handleDecrement.bind(this)} />
                     </div>
                 </div>
                 <div>
