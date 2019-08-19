7ce8130c8d8280b8fd605bfd0d6371a221d755dd
Priyadarshni Sivakumaran <priyadarshni.sivakumaran@emc.com>
2019-08-19 08:27:02 +0000

    Accordion component - first draft
    Use clarity-react v2

---
+++ src/accordian/ClassNames.tsx
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+export class ClassNames {
+    public static ACCORDION: string = "clr-accordion";
+    public static ACCORDION_HEADER: string = "clr-accordion-header";
+    public static ACCORDION_PANEL: string = "clr-accordion-panel ng-star-inserted ng-tns-c6-2";
+    public static ACCORDION_PANEL_INNER: string = "clr-accordion-panel-open ng-tns-c6-2 ng-star-inserted";
+    public static ACCORDION_TITLE: string = "clr-accordion-title";
+    public static ACCORDION_CONTENT: string = "clr-accordion-content ng-star-inserted";
+    public static ACCORDION_HEADER_BUTTON: string = "clr-accordion-header-button";
+    public static ACCORDION_STATUS: string = "clr-accordion-status";
+    public static ACCORDION_ANGLE: string = "clr-accordion-angle";
+    public static ACCORDION_INNER_CONTENT: string = "clr-accordion-inner-content";
+    public static ACCORDION_CONTENT_OUTER_DIV: string = "ng-tns-c6-2 ng-trigger ng-trigger-skipInitialRender";
+    public static SR_ONLY: string = "clr-sr-only";
+}
