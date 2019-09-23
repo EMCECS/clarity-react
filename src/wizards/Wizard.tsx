0e586da829b602d654f8243cca35f4d4999551c0
rajenv3 <vinod.wani@emc.com>
2019-09-23 10:11:47 +0530

    Added flag to show/hide wizrd header

--- src/wizards/Wizard.tsx
+++ src/wizards/Wizard.tsx
@@ -51,6 +51,7 @@ type WizardProps = {
     onClose?: Function;
     navLinkClasses?: string;
     validationType?: WizardValidationType;
+    showHeader?: boolean;
     showFooter?: boolean;
 };
 
@@ -90,6 +91,7 @@ export class Wizard extends React.PureComponent<WizardProps> {
         showNav: true,
         defaultStepId: 0,
         validationType: WizardValidationType.NONE,
+        showHeader: true,
         showFooter: true,
     };
 
@@ -436,7 +438,7 @@ export class Wizard extends React.PureComponent<WizardProps> {
     }
 
     private buildWizard(): React.ReactElement {
-        const {size, closable, steps, children, showFooter} = this.props;
+        const {size, closable, steps, children, showFooter, showHeader} = this.props;
         const wizardSize = "wizard-" + size;
         const modalSize = "modal-" + size;
 
@@ -454,21 +456,23 @@ export class Wizard extends React.PureComponent<WizardProps> {
                                 <div className={ClassNames.MODAL_CONTENT_WRAPPER}>
                                     {this.buildWizardNav()}
                                     <div className={ClassNames.MODAL_CONTENT}>
-                                        <div className={ClassNames.MODAL_HEADER}>
-                                            <button
-                                                aria-label="Close"
-                                                className="close"
-                                                type="button"
-                                                onClick={this.close.bind(this)}
-                                            >
-                                                <Icon aria-hidden={true} shape="close" />
-                                            </button>
-                                            <h3 className={ClassNames.MODAL_TITLE} style={Styles.MODAL_TITELE}>
-                                                <span className={ClassNames.MODAL_TITLE_TEXT}>
-                                                    {steps[this.state.currentStepId].stepName}
-                                                </span>
-                                            </h3>
-                                        </div>{" "}
+                                        {showHeader ? (
+                                            <div className={ClassNames.MODAL_HEADER}>
+                                                <button
+                                                    aria-label="Close"
+                                                    className="close"
+                                                    type="button"
+                                                    onClick={this.close.bind(this)}
+                                                >
+                                                    <Icon aria-hidden={true} shape="close" />
+                                                </button>
+                                                <h3 className={ClassNames.MODAL_TITLE} style={Styles.MODAL_TITELE}>
+                                                    <span className={ClassNames.MODAL_TITLE_TEXT}>
+                                                        {steps[this.state.currentStepId].stepName}
+                                                    </span>
+                                                </h3>
+                                            </div>
+                                        ) : null}{" "}
                                         {/*Close modal-header */}
                                         <div className={ClassNames.MODAL_BODY}>{this.buildWizardSteps()}</div>{" "}
                                         {/*Close modal-body*/}
