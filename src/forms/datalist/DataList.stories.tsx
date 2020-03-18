d9c86be28efecb600e228c13d82f22e0fbfa07cf
katea <arpita.kate@emc.com>
2020-03-18 22:12:42 +0530

    Added datalist component

--- src/forms/datalist/DataList.stories.tsx
+++ src/forms/datalist/DataList.stories.tsx
@@ -10,17 +10,57 @@
 
 import * as React from "react";
 import {storiesOf} from "@storybook/react";
-import {DataList} from "./DataList";
+import {DataList, DataListOption} from ".";
 import {action} from "@storybook/addon-actions";
 
 storiesOf("DataList", module)
     .add("Basic DataList", () => (
         <div>
-            <DataList />
+            <DataList>
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
         </div>
     ))
     .add("With place holder text", () => (
         <div>
-            <DataList placeHolder="No label" />
+            <DataList placeHolder="No label">
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With Error", () => (
+        <div>
+            <DataList placeHolder="No label" required={true} isError={true} errorText="This field is reuired">
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With helper text", () => (
+        <div>
+            <DataList placeHolder="No label" helperText="Select any option or create one">
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
+        </div>
+    ))
+    .add("With custom width and label", () => (
+        <div>
+            <DataList
+                placeHolder="No label"
+                helperText="Select any option or create one"
+                label="Select Item"
+                style={{width: "50%"}}
+            >
+                <DataListOption value="Item1" />
+                <DataListOption value="Item2" />
+                <DataListOption value="Item3" />
+            </DataList>
         </div>
     ));
