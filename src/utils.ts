dee02366bb710b77c01d9228102ff9b484c04bdc
Bhawani Singh Bhati <78294852+Bhatii@users.noreply.github.com>
2022-06-03 17:14:50 +0530

    [OBSDEF-15071] Update formatting

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 
-export type ReactChildren = React.ReactNode | (React.ReactNode[] & React.ReactNode)
+export type ReactChildren = React.ReactNode | (React.ReactNode[] & React.ReactNode);
 
 export function classNames(classNameList: (false | undefined | null | string)[]) {
     return classNameList.filter(x => typeof x === "string").join(" ");
